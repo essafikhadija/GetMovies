28ebc5d71eb33b0cf289f07aa978de7c880ed00a
khadija <khadi@gma.co>
2020-01-14 15:00:29 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./containers/app";
+
+const API_KEY = "ee52528a3d2bfff0312880daeaee21b3";
+
+
+ReactDom.render(<App/>,document.querySelector('.container'))
